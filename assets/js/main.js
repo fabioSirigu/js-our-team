85f425c296be11beed798ef366b69268ce8c348c
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-20 12:51:54 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1: Trasformare la stringa foto in una immagine effettiva
+// BONUS 2: Organizzare i singoli membri in card/schede
+/* Name: Wayne Barnett
+Role: Founder & CEO
+Image: wayne-barnett-founder-ceo.jpg
+___________________________________
+Name: Angela 
+Role: Chief Editor
+Image: angela-caroll-chief-editor.jpg
+____________________________________
+Name: Waleter gordon
+Role: Chief Editor
+Image: walter-gordon-office-manager.jpg
+____________________________________
+Name: Waleter gordon
+Role: Social Media Manager
+Image: angela-lopez-social-media-manager.jpg
+_________________________________
+Name: Scott Estrada
+Role: Developer
+Image: scott-estrada-developer.jpg
+_________________________________
+Name: Barbara Ramos
+Role: Grafic designer
+Image: barbara-ramos-graphic-designer.jpg */
